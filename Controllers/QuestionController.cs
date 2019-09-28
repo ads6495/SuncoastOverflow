61c8bd88e34716920c0e8e3a3695dafdf9ea3fdf
ads6495 <astrange55@icloud.com>
2019-09-28 15:44:30 -0400

    finished answer controller

--- Controllers/QuestionController.cs
+++ Controllers/QuestionController.cs
@@ -1,7 +1,47 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using suncoastoverflow;
+using SuncoastOverflow.Models;
+using static Microsoft.AspNetCore.Hosting.Internal.HostingApplication;
+
 namespace SuncoastOverflow.Controllers
 {
-    public class QuestionController
+  [Route("api/[controller]")]
+  [ApiController]
+  public class QuestionController : ControllerBase
+  {
+    private DatabaseContext context;
+    public QuestionController(DatabaseContext _context)
     {
-        
+      this.context = _context;
     }
+    [HttpGet("GetAll")]
+    public ActionResult<IEnumerable<QuestionPost>> GetAll()
+    {
+      var AllQuestions = context.QuestionPosts.OrderBy(q => q.Id);
+      return AllQuestions.ToList();
+    }
+
+
+
+    [HttpGet(AllAnswersJoin{Id})]
+    public ActionResult<IEnumerable<Object>> GetAnswers(Id)
+  {
+    var QuestionReturned = Context.QuestionPosts.Join(context.AnswerPosts, i => i.Id, l => l.QuestionPostId, (i, l) => new
+    {
+
+      QuestionId = i.Id,
+      QuestionDescription = i.Description,
+
+
+    })
+
+    }
+
+
+
+
+
+}
 }
